2fbd0cb2e16c2c8160a13c1e2a82a5bd13f61e0a
sani0928 <kksan12@gmail.com>
2025-04-07 15:18:08 +0900

    [Gold III] Title: 전기가 부족해, Time: 3548 ms, Memory: 47528 KB -BaekjoonHub

---
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.py
@@ -0,0 +1,49 @@
+# 모든 정점의 부모가 spot 중 하나에 포함되어야 한다.
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a,b,w):
+    global cost
+
+    root_a = find(a)
+    root_b = find(b)
+
+    if root_a == root_b:
+        return
+
+    if root_a in spot and root_b in spot:
+        return
+
+    cost += w
+
+
+    if root_a in spot:
+        parent[root_b] = root_a
+
+    else:
+        parent[root_a] = root_b
+
+N, M, K = map(int,input().split())
+spot = list(map(int,input().split()))
+edges = []
+parent = [i for i in range(N+1)]
+cost = 0
+
+for _ in range(M):
+    u,v,w = map(int,input().split())
+    edges.append((w,u,v))
+
+edges.sort()
+
+for w,u,v in edges:
+
+    if find(u) != find(v):
+        union(u,v,w)
+
+
+    if all(find(parent[i]) in spot for i in range(1, N+1)):
+        print(cost)
+        break
